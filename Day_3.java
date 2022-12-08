78c65905a4cbc94ba9592c81538881224ce0c767
Adam <72197966+Adamstevo42@users.noreply.github.com>
2022-12-08 16:03:10 +0000

    Create Day_3.java

---
+++ Day_3.java
@@ -0,0 +1,18 @@
+public class Day_3 {
+
+  public static void main(String args[]) throws Exception {
+
+    public static int calc_priority(char c){
+      if (Character.isLowerCase(c)){//ascii values are 97 to 122
+        return (int) c - 96;
+      } else {// is an upper case character
+        return (int) c - 38;
+      }
+    }
+    
+
+
+    //exactly one item type in each rucksack that is in both compartments
+
+  }
+}
